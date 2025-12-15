8d0dbd6881e1b0f4b696aa0f6180b71cc1a1bc59
Higor Silva <higor.junio@hotmail.com>
2025-12-15 11:05:09 -0300

    feat: add rotas para inquilinos

--- routes/web.php
+++ routes/web.php
@@ -39,6 +39,13 @@ Route::middleware(['auth'])->group(function () {
         Volt::route('/{inquilino}/edit', 'pages.inquilinos.edit')->name('edit');
         Volt::route('/{inquilino}', 'pages.inquilinos.show')->name('show');
     });
+
+    Route::prefix('locacoes')->name('locacoes.')->group(function () {
+        Volt::route('/', 'pages.locacoes.index')->name('index');
+        Volt::route('/create', 'pages.locacoes.create')->name('create');
+        Volt::route('/{inquilino}/edit', 'pages.locacoes.edit')->name('edit');
+        Volt::route('/{inquilino}', 'pages.locacoes.show')->name('show');
+    });
 });
 
 require __DIR__ . '/auth.php';
