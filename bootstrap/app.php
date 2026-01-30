c5460f15ad49b67cb48732db326371bec94d8e2f
Higor Silva <higor.junio@hotmail.com>
2026-01-30 13:24:29 -0300

    deat: adiciona observabilidade com sentry

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,6 +6,7 @@ use App\Http\Middleware\Localization;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Sentry\Laravel\Integration;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -17,5 +18,5 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->append(Localization::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        Integration::handles($exceptions);
     })->create();
